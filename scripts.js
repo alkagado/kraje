c13db6fa65b196f5bae39af5d431842c17c45c30
alkagado <38248590+alkagado@users.noreply.github.com>
2018-05-10 11:46:09 +0200

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,28 @@
+var url = 'https://restcountries.eu/rest/v1/name/';
+var countriesList = $('#countries');
+$('#search').click(searchCountries);
+
+function searchCountries() {
+  	var countryName = $('#country-name').val();
+if(!countryName.length) countryName = 'Poland';
+}
+
+
+
+$.ajax({
+	url: "https://restcountries.eu/rest/v2/name/{name}",
+	method: "GET",
+	success: function searchCountries() {
+ 	var countryName = $('#country-name').val();
+if(!countryName.length) countryName = 'Poland';
+$.ajax({
+  		url: url + countryName,
+  		method: 'GET',
+  		success: showCountriesList
+  	});
+}});
+
+
+function showCountriesList(resp) {
+  countriesList.empty();
+}
