40dc327b999882a0a9f63a7c8279c1580c3d0d4f
alkagado <38248590+alkagado@users.noreply.github.com>
2018-05-10 21:39:31 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,12 +5,14 @@ $('#search').click(searchCountries);
 
 function searchCountries() {
  	var countryName = $('#country-name').val();
-if(!countryName.length) countryName = 'Poland';
+if(!countryName.length) {countryName = 'Poland';}
+ 
+ 
 $.ajax({
-  		url: "https://restcountries.eu/rest/v2/name/{name}" + countryName,
+  		url: url + countryName,
   		method: 'GET',
   		success: showCountriesList
-  	});
+  	})
 }
 
 
